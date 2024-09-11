64c454de2f82ba6748d87eaa7631e62425217af3
scalevvillain <shihki@inbox.ru>
2024-09-12 00:56:00 +0300

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <locale.h>
+void main()
+{
+
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("                                      ");
+	    puts("           \\            /              ");
+	    puts("            \\          /           ");
+	    puts("             \\________/               ");
+	    puts("              |       |                  ");
+	    puts("             /|       |\\                ");
+	    puts("            / |_______| \\                       ");
+	    puts("           / /        \\  \\             ");
+	    puts("          / /          \\  \\              ");
+	    puts("           /             \\             ");
+	    puts("                              _        ");
+	    puts("                           _ |_|       ");
+	    puts("                        _ |_|           ");
+	    puts("                     __|_|                    ");
+	    puts("                    /__\\                ");
+	    puts("                    |  |               ");
+		puts("                  __|__|___             ");
+		puts("                 /  __     \\           ");
+		puts("                /  |__|     \\          ");
+		puts("               /             \\         ");
+		puts("              |              |            ");
+		puts("              |        ____  |             ");
+		puts("              |       |    | |           ");
+		puts("              |       |  | | |            ");
+		puts("              |_______|____|_|                        ");
+		
+
+
+
+}
